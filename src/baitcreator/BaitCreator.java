c88c182a266a8f2ce79abf6ae16349975661e705
jason <jason@jDesktop>
2017-05-13 13:49:05 -0400

    Add initial pane setup (hbox top, vbox@leftSide, StackPane center)

--- src/baitcreator/BaitCreator.java
+++ src/baitcreator/BaitCreator.java
@@ -15,10 +15,14 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -37,21 +41,34 @@ public class BaitCreator extends Application {
         /*Horizontal Box for Steps*/
         HBox hBoxForStep = new HBox(5);
         hBoxForStep.setAlignment(Pos.CENTER);
-        Button step1 = new Button("Step 1");
-        Button step2 = new Button("Step 2");
-        Button step3 = new Button("Step 3");
-        hBoxForStep.getChildren().addAll(step1,step2,step3);
+        hBoxForStep.setPadding(new Insets(10,10,10,10));
+        Text title = new Text("Steps: ");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        Button step1 = new Button("1: Choose Base"); //todo: color and weight
+        Button step2 = new Button("2: Choose (option)");
+        Button step3 = new Button("3: Choose (option)");
+        hBoxForStep.getChildren().addAll(title,step1,step2,step3);
         
         /*Vertical Box for Bait Base*/
-        VBox vBoxForBait = new VBox(10);
-        vBoxForBait.setAlignment(Pos.CENTER);
+        VBox vBoxForBait = new VBox(1);
+        vBoxForBait.setAlignment(Pos.TOP_CENTER);
+        vBoxForBait.setPadding(new Insets(10));
+        vBoxForBait.setSpacing(10);
+        
+        Text baseTitle = new Text("Bait Base");
+        baseTitle.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        vBoxForBait.setStyle("-fx-border-color: black; -fx-background-color: lightgray;");
+        vBoxForBait.getChildren().addAll(baseTitle);
         for (int i=0; i<2; i++){
             vBoxForBait.getChildren().add(new ImageView("image/" + list.get(i) + ".png"));
         }
         
         /*Stack Pane for Bait-in-Creation area*/
         StackPane paneForBait = new StackPane();
-        paneForBait.setPadding(new Insets(50));
+        paneForBait.setAlignment(Pos.CENTER);
+        paneForBait.setPadding(new Insets(15));
+        paneForBait.getChildren().add(new ImageView("image/q.png"));
+        
         
         /*Border Pane*/
         BorderPane borderPane = new BorderPane();
